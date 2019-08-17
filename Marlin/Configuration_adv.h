23b5899a5f02612b27618b69fce553b47591e8a1
Belfaigore <belfaigore@gmail.com>
2019-08-18 00:25:40 +0200

    Mise à jour des pin

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -262,7 +262,7 @@
  */
 #define CASE_LIGHT_ENABLE
 #if ENABLED(CASE_LIGHT_ENABLE)
-  #define CASE_LIGHT_PIN 44                    // Override the default pin if needed AUX2-D40
+  #define CASE_LIGHT_PIN 44                    // Override the default pin if needed AUX2-D44 (PWM)
   #define INVERT_CASE_LIGHT false             // Set true if Case Light is ON when pin is LOW
   #define CASE_LIGHT_DEFAULT_ON false         // Set default power-up state on
   #define CASE_LIGHT_DEFAULT_BRIGHTNESS 255   // Set default power-up brightness (0-255, requires PWM pin)
